e3ad9c403ee6a978294b0bab8848e2e5c7083213
akimov246 <akimov246@yandex.ru>
2025-08-09 00:15:16 +0700

    Сетевые запросы. 9. Возобновляемая загрузка файлов

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, Form, File, UploadFile
+from fastapi import FastAPI, Form, File, UploadFile, Header
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse, JSONResponse
 from starlette.requests import Request
+from pathlib import Path
+import os
+import time
 
 app = FastAPI()
 
@@ -35,7 +38,7 @@ async def article_xmlhttprequest_post_json(request: Request):
     json = await request.json()
     print(json.get('name'))
     return {
-        'message': f'Ответ от сервера\nПользователь: "{json.get('surname')} {json.get('name')}" сохранен'
+        'message': f'Ответ от сервера\nПользователь: "{json.get("surname")} {json.get("name")}" сохранен'
     }
 
 @app.post('/article/xmlhttprequest/post/upload')
@@ -43,4 +46,42 @@ async def article_xmlhttprequest_post_upload(file: UploadFile = File(...)):
     content = await file.read()
     return {
         'filename': file.filename
+    }
+
+UPLOAD_DIR = Path('upload')
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+@app.get('/article/resume-upload/status')
+async def article_resume_upload_status(x_file_id: str = Header(..., alias='X-File-Id')):
+    print(x_file_id)
+    save_path = UPLOAD_DIR/f'{x_file_id}'
+    if save_path.exists():
+        return os.path.getsize(save_path)
+    else:
+        return 0
+
+@app.post('/article/resume-upload/upload')
+async def article_resume_upload_upload(
+        request: Request,
+        x_file_id: str = Header(..., alias='X-File-Id')
+):
+
+    save_path = UPLOAD_DIR/f'{x_file_id}'
+
+    mode = 'ab' if save_path.exists() else 'wb'
+
+    try:
+        with open(save_path, mode) as f:
+            async for chunk in request.stream():
+                f.write(chunk)
+    except:
+        print(f'Something went wrong!\nЗагружено: {os.path.getsize(save_path)}')
+        return {
+            "status": "aborted",
+            "size": os.path.getsize(save_path)
+        }
+
+    return {
+        "status": "OK",
+        "size": os.path.getsize(save_path)
     }
