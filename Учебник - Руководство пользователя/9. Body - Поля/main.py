5d00810063215f424af06b1f0ffe8d95cfe54e34
akimov246 <akimov246@yandex.ru>
2025-11-08 22:26:55 +0700

    Учебник - Руководство пользователя. 9. Body - Поля

---
+++ Учебник - Руководство пользователя/9. Body - Поля/main.py
@@ -0,0 +1,27 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class Item(BaseModel):
+    name: str
+    description: Annotated[str | None, Field(title="The description of the item", max_length=300)] = None
+    price: Annotated[float, Field(gt=0, description="The price must be greater than zero")]
+    tax: float | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
