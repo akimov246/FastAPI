7c1da6a9056511994e43d9c25114c630a7934af5
akimov246 <akimov246@yandex.ru>
2025-11-10 23:25:17 +0700

    Учебник - Руководство пользователя. 11. Объявление примеров данных запроса

---
+++ Учебник - Руководство пользователя/11. Объявление примеров данных запроса/main.py
@@ -0,0 +1,82 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: Annotated[str, Field(examples=["Foo"])]
+    description: Annotated[str | None, Field(examples=["A very nice Item"])] = None
+    price: Annotated[float, Field(examples=[35.4])]
+    tax: Annotated[float | None, Field(examples=[3.2])] = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2
+                }
+            ]
+        }
+    }
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+        item_id: int,
+        item: Annotated[
+            Item,
+            Body(
+                examples=[
+                    {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                    {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                    {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                ],
+                openapi_examples={
+                    "normal": {
+                        "summary": "A normal example",
+                        "description": "A **normal** item works correctly.",
+                        "value": {
+                            "name": "Foo",
+                            "description": "A very nice Item",
+                            "price": 35.4,
+                            "tax": 3.2,
+                        },
+                    },
+                    "converted": {
+                        "summary": "An example with converted data",
+                        "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                        "value": {
+                            "name": "Bar",
+                            "price": "35.4",
+                        },
+                    },
+                    "invalid": {
+                        "summary": "Invalid data is rejected with an error",
+                        "value": {
+                            "name": "Baz",
+                            "price": "thirty five point four",
+                        },
+                    }
+                }
+            )
+        ]
+):
+    results = {"item_id": item_id, "item": item}
+    return results
